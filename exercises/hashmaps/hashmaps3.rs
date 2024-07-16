c2ad086674ed6e9f109721b81aff7cc6820653c3
Tomori0Nao <zjhzjhanpanman@gmail.com>
2024-07-16 11:16:15 +0800

    67 finished

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,7 +14,6 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -39,6 +38,19 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+
+        if scores.contains_key(&team_1_name) {
+            let current_team = scores.get(&team_1_name).unwrap();
+            scores.insert(team_1_name, Team {goals_scored: team_1_score + current_team.goals_scored , goals_conceded: team_2_score + current_team.goals_conceded});
+        } else {
+            scores.insert(team_1_name, Team {goals_scored: team_1_score , goals_conceded: team_2_score});
+        }
+        if scores.contains_key(&team_2_name) {
+            let current_team = scores.get(&team_2_name).unwrap();
+            scores.insert(team_2_name, Team {goals_scored: team_2_score + current_team.goals_scored,goals_conceded:team_1_score + current_team.goals_conceded} );
+        } else {
+            scores.insert(team_2_name, Team {goals_scored: team_2_score , goals_conceded: team_1_score} );
+        }
     }
     scores
 }
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(apples_num:i32) ->i32 {
+    if apples_num <= 40 {
+        apples_num * 2
+    } else {
+        apples_num
+    }
+}
 
 // Don't modify this function!
 #[test]
